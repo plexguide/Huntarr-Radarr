149179da0ea94b7c90f82200238a0a1bf13667f8
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2025-04-03 11:04:28 -0400

    Update huntarr.sh

--- huntarr.sh
+++ huntarr.sh
@@ -403,6 +403,6 @@ while true; do
     echo "State reset is disabled. Processed items will be remembered indefinitely."
   fi
 
-  echo "Like the tool? Donate toward my daughter's college fund via donate.plex.one and make her day!"
+  echo "⭐ Enjoy the Tool? Donate @ https://donate.plex.one towards my Daughter's 501 College Fund!"
   sleep "$SLEEP_DURATION"
 done
